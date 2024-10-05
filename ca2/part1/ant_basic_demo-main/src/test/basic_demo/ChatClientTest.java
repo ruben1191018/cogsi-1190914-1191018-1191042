a537f0275463ad0d05a80e323db197835dd96c4e
Nuno Domingues <1190914@isep.ipp.pt>
2024-10-05 17:10:10 +0100

    #22 Add unit test and unit test task to the ant file

---
+++ ca2/part1/ant_basic_demo-main/src/test/basic_demo/ChatClientTest.java
@@ -0,0 +1,12 @@
+package basic_demo;
+
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ChatClientTest {  // Class must be public
+
+    @Test  // JUnit 5 annotation for a test method
+    public void run() {  // Method must be public
+        assertTrue(true);  // Simple test assertion
+    }
+}
