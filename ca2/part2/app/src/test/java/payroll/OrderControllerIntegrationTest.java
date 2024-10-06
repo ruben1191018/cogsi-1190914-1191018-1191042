82e8fba622cdf8c665a9068eb9e0644877eafa39
Rui Pinto <66411769+1191042@users.noreply.github.com>
2024-10-06 11:34:39 +0100

    #20 Fix integration test

--- ca2/part2/app/src/test/java/payroll/OrderControllerIntegrationTest.java
+++ ca2/part2/app/src/test/java/payroll/OrderControllerIntegrationTest.java
@@ -5,28 +5,23 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.hateoas.EntityModel;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Arrays;
-
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class OrderControllerTest {
+class OrderControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @Autowired
     private OrderRepository orderRepository;
 
-    @MockBean
+    @Autowired
     private OrderModelAssembler assembler;
 
     private Order order1;
@@ -34,7 +29,8 @@ class OrderControllerTest {
 
     @BeforeEach
     void setUp() {
-        // Initialize sample Order objects
+        orderRepository.deleteAll();
+
         order1 = new Order();
         order1.setId(1L);
         order1.setStatus(Status.IN_PROGRESS);
@@ -44,18 +40,13 @@ class OrderControllerTest {
         order2.setId(2L);
         order2.setStatus(Status.COMPLETED);
         order2.setDescription("Order 2");
+
+        orderRepository.save(order1);
+        orderRepository.save(order2);
     }
 
     @Test
     void testGetAllOrders() throws Exception {
-        // Mock the order repository to return a list of orders
-        when(orderRepository.findAll()).thenReturn(Arrays.asList(order1, order2));
-
-        // Mock the assembler behavior to convert each order into an EntityModel
-        when(assembler.toModel(order1)).thenReturn(EntityModel.of(order1));
-        when(assembler.toModel(order2)).thenReturn(EntityModel.of(order2));
-
-        // Perform GET request and validate the response
         mockMvc.perform(get("/orders")
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
