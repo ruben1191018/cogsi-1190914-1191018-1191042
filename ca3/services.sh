18584a3e40e9b26e4a8f12741a968c6ebcc65ba0
Rui Pinto <66411769+1191042@users.noreply.github.com>
2024-10-26 18:41:07 +0100

    #31 Automate cloning

---
+++ ca3/services.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+REPO_URL=https://github.com/ruben1191018/cogsi-1190914-1191018-1191042.git
+
+git clone $REPO_URL
+
+cd cogsi-1190914-1191018-1191042/ca2/part1/gradle_basic_demo-main
+
+gradle build
+
