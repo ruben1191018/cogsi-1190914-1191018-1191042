ce43e0afc5a77a0577dbcf3edfd5119ab398bab4
rodriguesr5 <ruben.rodrigues@paddypowerbetfair.com>
2024-10-27 16:02:23 +0000

    #32 Add h2 persistence after restart

--- ca3/requirements.sh
+++ ca3/requirements.sh
@@ -23,6 +23,45 @@ wget https://services.gradle.org/distributions/gradle-8.4-bin.zip -P /tmp
 sudo unzip -d /opt/gradle /tmp/gradle-8.4-bin.zip
 sudo ln -s /opt/gradle/gradle-8.4/bin/gradle /usr/bin/gradle
 
+wget https://github.com/h2database/h2database/releases/download/version-2.3.230/h2-2024-07-15.zip -P /tmp
+sudo unzip -d /home/vagrant/ /tmp/h2-2024-07-15.zip
+
+# Create a script to run the H2 server with disk storage
+sudo cat <<EOF > start_h2.sh
+#!/bin/bash
+java -cp /home/vagrant/h2/bin/h2*.jar org.h2.tools.Server -tcp -tcpAllowOthers -baseDir /home/vagrant/h2data
+EOF
+
+
+# Make the script executable
+chmod +x start_h2.sh
+
+
+# Create a systemd service file for H2 if it doesn't exist
+cat <<EOF | sudo tee /etc/systemd/system/h2.service
+[Unit]
+Description=H2 Database Service
+After=network.target
+
+[Service]
+ExecStart=/home/vagrant/start_h2.sh
+User=vagrant
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload systemd to recognize the new service, enable it, and start it
+sudo systemctl daemon-reload
+sudo systemctl enable h2
+sudo systemctl start h2
+
+sleep 5s
+
+sudo systemctl status h2
+
+
 # Verify installations
 source /etc/profile.d/jdk.sh  # Make sure JAVA_HOME and PATH are available in the current shell
 
